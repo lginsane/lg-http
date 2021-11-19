7c680618f0dc6a3fbf3cbcf9e1090504877c7347
梁傲 <1289206613@qq.com>
2021-11-19 18:55:59 +0800

    fix: 初始化配置参数

--- packages/types/index.js
+++ packages/types/index.js
@@ -4,20 +4,27 @@ export const defaultOptions = {
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
     },
+    // 加载
     showLoading: true,
     loadingMethods: {
         open: () => { console.log('打开loading') },
         close: () => { console.log('关闭loading') }
     },
+    // 提示
     showToast: true,
     toastMethods: console.log,
     defaultErrorToastMessage: '服务异常，请重新再试',
+    // 成功状态
     successRequestAssert: function(res) {
         return res.code === 200
     },
+    // 是否优化重复请求
+    isOptimization: false,
+    // 是否刷新token
     isRefreshToken: false,
     refreshRequestAssert: function(res) {
         return res.code === 401
     },
-    requestAssert: {}
+    // 根据code处理特殊情况
+    requestAssert: function(res) {}
 }
