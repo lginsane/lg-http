9b59342b8a07bda47742eddf89aa5516bb5e7430
梁傲 <1289206613@qq.com>
2021-11-19 18:56:33 +0800

    fix: toast

--- packages/http/toast.js
+++ packages/http/toast.js
@@ -1,4 +1,39 @@
 /* 消息提示 */
-export const toast = {
-    
+import { isObject, isFunction } from '../utils/is'
+const Toast = {
+    methods: null,
+    defaultMessage: '',
+    init({ toastMethods, message, showToast }) {
+        this._methods = toastMethods
+        this._defaultMessage = message
+        this._showToast = showToast
+    },
+    _toast(msg, cb) {
+        cb && cb(msg)
+    },
+    getShowToast(attaches) {
+        return this._showToast || (attaches && attaches.showToast)
+    },
+    success(attaches) {
+        const successMessage = attaches && attaches.successMessage
+        if (this.getShowToast(attaches) && successMessage) {
+            this._toast(successMessage, this._methods.success)
+        }
+    },
+    error(response, attaches) {
+        const errorMessage = response ? response.message || response.msg : ''
+        let method
+        if (isObject(this._methods)) {
+            if (isFunction(this._methods.error)) {
+                method = this._methods.error
+            }
+        } else if (isFunction(this._methods)) {
+            method = this._methods
+        }
+        if (this.getShowToast(attaches)) {
+            this._toast(errorMessage || this._defaultMessage, method)
+        }
+    }
 }
+
+export default Toast
