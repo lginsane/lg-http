85c968f143d6efa0b250d98bcd8426b882ed57f2
梁傲 <1289206613@qq.com>
2021-11-22 14:55:07 +0800

    fix: 取消请求

--- packages/http/cancel.js
+++ packages/http/cancel.js
@@ -1,27 +1,42 @@
 /* 处理取消重复请求 */
 import { CancelToken } from 'axios'
 
-export const cancelQueue = {
+const cancelQueue = {
+    _isOptimization: false,
     list: {},
     CancelToken: CancelToken,
-    add(name, cancel) {
+    init({ isOptimization }) {
+        this._isOptimization = isOptimization
+    },
+    getOptimization(attaches) {
+        return this._isOptimization || (attaches && attaches.isOptimization)
+    },
+    add(name, cancel, attaches) {
+        if (!this.getOptimization(attaches)) return
+        if (this.list[name]) {
+            this.list[name](msg)
+            delete this.list[name]
+        }
         this.list[name] = msg => {
             cancel(msg)
         }
     },
-    remove(name) {
+    cancel(name, attaches) {
+        if (!this.getOptimization(attaches)) return
         if (this.list[name]) {
             delete this.list[name]
         }
     },
-    cancel(name, msg = '') {
-        if (this.list[name]) {
-            this.list[name](msg)
-        }
-    },
-    cancelAll(msg = '') {
+    cancelAll(msg = '', attaches) {
+        if (!this.getOptimization(attaches)) return
         Object.keys(this.list).forEach(key => {
             this.list[key](msg)
         })
+    },
+    clear(attaches) {
+        if (!this.getOptimization(attaches)) return
+        this.list = {}
     }
 }
+
+export default cancelQueue
