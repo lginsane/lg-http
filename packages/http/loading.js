fa9e91fbc43b7b83f3866f561f4316a000419623
梁傲 <1289206613@qq.com>
2021-11-22 14:57:46 +0800

    fix: 请求加载效果

--- packages/http/loading.js
+++ packages/http/loading.js
@@ -1,4 +1,28 @@
 /* 加载效果 */
-export const Loading = {
-    
+import { isFunction } from '../utils/is'
+
+const Loading = {
+    _methods: null,
+    _showLoading: false,
+    init({ showLoading, loadingMethods }) {
+        this._methods = loadingMethods
+        this._showLoading = showLoading
+    },
+    getShowLoading(attaches) {
+        return this._showLoading || (attaches && attaches.showLoading)
+    },
+    open(attaches) {
+        if (!this.getShowLoading(attaches)) return
+        if (this._methods && this._methods.open && isFunction(this._methods.open)) {
+            this._methods.open()
+        }
+    },
+    close(attaches) {
+        if (!this.getShowLoading(attaches)) return
+        if (this._methods && this._methods.close && isFunction(this._methods.close)) {
+            this._methods.close()
+        }
+    }
+
 }
+export default Loading
