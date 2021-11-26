fc959e2dc6f7433c2e6c1fbc9f423955a26e320d
梁傲 <1289206613@qq.com>
2021-11-26 18:57:43 +0800

    fix: 采用内置方法调用接口

--- packages/hooks/useHttp.js
+++ packages/hooks/useHttp.js
@@ -1,35 +1,20 @@
 import { ref, shallowRef } from 'vue'
-import axios from 'axios'
+import Http from '@@/http/index'
 
-export function useHttp(url, ...args) {
-    let instance = axios
-    let config = {}
-    let cancel = null
-    if (args.length > 0) {
-        if ('request' in args[0]) instance = args[0]
-        else config = args[0]
-    }
+export function useHttp(option, { url, data: requestData, attaches, config }) {
+    const $http = new Http(option)
+    const method = (config && config.method) || 'get'
 
-    if (args.length > 1) {
-        if ('request' in args[1]) instance = args[1]
-    }
-    config.cancelToken = new axios.CancelToken(function executor(c) {
-        cancel = c
-    })
     const response = shallowRef()
     const data = shallowRef()
     const isFinished = ref(false)
     const isLoading = ref(true)
-    const aborted = ref(false)
     const error = shallowRef()
-    const abort = (message) => {
-        if (!cancel || isFinished.value || !isLoading.value) return
-        cancel(message)
-        aborted.value = true
-        isLoading.value = false
-        isFinished.value = false
+    const _attaches = {
+        showLoading: false,
+        ...attaches
     }
-    instance(url, config)
+    $http[method](url, requestData, _attaches, config)
         .then((r) => {
             response.value = r
             data.value = r.data
@@ -48,10 +33,6 @@ export function useHttp(url, ...args) {
         finished: isFinished,
         loading: isLoading,
         isFinished,
-        isLoading,
-        cancel: abort,
-        canceled: aborted,
-        aborted,
-        abort
+        isLoading
     }
 }
